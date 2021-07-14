6598bc143a371ef0b5fa6885cd42d792d9cffac3
Coder-prathamesh <87426098+Coder-prathamesh@users.noreply.github.com>
2021-07-14 13:50:52 +0100

    Load the dataset

--- cars.py
+++ cars.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+from plotly import __version__
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot 
+# Read the dataset from local computer
+Cars = pd.read_csv('Automobile_data.csv')
+Cars.info()
 
